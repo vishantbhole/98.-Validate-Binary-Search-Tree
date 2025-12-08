caeea022474add8ca37755b15b916a3180ca5131
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-12-07 22:20:17 -0600

    Update isValidBST.py

--- isValidBST.py
+++ isValidBST.py
@@ -16,3 +16,8 @@ class Solution:
         def valid(node,left,right):
             if not node:
                 return True
+            if not(node.val < right and node.val > left):
+                return False
+
+            return (valid(node.left, left, node.val) and
+                    valid(node.right,node.val, right))
