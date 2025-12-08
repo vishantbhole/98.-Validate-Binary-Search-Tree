f186b5a6381c18d411ea0a58e37c68d1db5f6d06
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-12-07 22:18:58 -0600

    Add TreeNode class definition for BST validation

--- isValidBST.py
+++ isValidBST.py
@@ -1,2 +1,10 @@
 # 98. Validate Binary Search Tree
 # Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
